228c6488418f40e21f7a71b484091221001a5394
Quinten Clause <quinten.clause@studioemma.eu>
2019-06-18 19:52:37 +0200

    Correct formatting of git log

--- index.js
+++ index.js
@@ -2,21 +2,37 @@
 
 const fs = require('fs-extra');
 const util = require('util');
-const exec = util.promisify(require('child_process').exec);
+const { execSync } = require('child_process');
+
+const contentDir = './content/';
+
+function padTime(num) {
+    return (('' + num).length == 2) ? ('' + num) : ('0' + num);
+}
 
 fs.emptyDir('pub')
 .then(() => {
     console.log('Empty pub dir success...');
-    exec(`git log --pretty=format:'{"hash":"%H","date":"%aI","subject":"%s","body":"%b","author":{"name":"%an","email":"%ae"}},' ./content/the-very-first-test-scrap.png`)
-    .then(({stdout, stderr}) => {
-        if (stderr) {
-            console.log(stderr);
+    let posts = [];
+    fs.readdirSync(contentDir).forEach(file => {
+        let contentFile = contentDir + file;
+        let gitlog = execSync(`git log --pretty=format:'{"hash":"%H","date":"%aI","subject":"%s","body":"%b","author":{"name":"%an","email":"%ae"}},' ${contentFile}`);
+        gitlog = gitlog.toString();
+        if (!gitlog) {
             return;
         }
-        console.log(stdout);
-    })
-    .catch(err => {
-        console.log(err);
+        gitlog = gitlog.slice(0, -1);
+        gitlog = gitlog.replace('\n', '');
+        gitlog = '[' + gitlog + ']';
+        gitlog = JSON.parse(gitlog);
+        gitlog = gitlog[0];
+        gitlog.file = file; // todo: allow multiple files
+        let dateObj = new Date(gitlog.date);
+        gitlog.timestamp = dateObj.getTime();
+        gitlog.day = dateObj.toDateString();
+        gitlog.time = padTime(dateObj.getHours()) + ':' + padTime(dateObj.getMinutes());
+        console.log(gitlog);
+        posts.push(gitlog);
     });
 })
 .catch(err => {
