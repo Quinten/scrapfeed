5c6b4223ef9b9ae7a8d7451b3873768b7664a3b5
Quinten Clause <quinten.clause@studioemma.eu>
2019-06-18 17:11:52 +0200

    Add git log to json test command

--- index.js
+++ index.js
@@ -1,14 +1,26 @@
 #!/usr/bin/env node
 
 const fs = require('fs-extra');
+const util = require('util');
+const exec = util.promisify(require('child_process').exec);
 
 fs.emptyDir('pub')
 .then(() => {
     console.log('Empty pub dir success...');
+    exec(`git log --pretty=format:'{"hash":"%H","date":"%aI","subject":"%s","body":"%b","author":{"name":"%an","email":"%ae"}},' ./content/the-very-first-test-scrap.png`)
+    .then(({stdout, stderr}) => {
+        if (stderr) {
+            console.log(stderr);
+            return;
+        }
+        console.log(stdout);
+    })
+    .catch(err => {
+        console.log(err);
+    });
 })
 .catch(err => {
   console.error(err)
 });
 
-console.log('Done!');
 //console.log(process.cwd());
